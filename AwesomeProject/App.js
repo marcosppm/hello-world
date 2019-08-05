b854d35d2ab5eb49f875de10e588020b3c785a80
marcosppm <marcos_ppm@live.com>
2019-08-05 16:24:23 -0300

    Verificações de login finalizadas.

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -2,6 +2,37 @@ import React, { Component } from 'react';
 import { Text, View, TextInput, Button } from 'react-native';
 
 export default class HelloWorldApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {email:"", password:"", errorMessage:""};
+  }
+
+  validateEmailAndPassword() {
+    let regexEmail = /([A-Za-z])+@([A-Za-z])+.com/gm;
+    let regexOneCharAndOneDigit = /(?=.*?[0-9])(?=.*?[A-Za-z]).+/gm;
+    if (this.state.email.length == 0) {
+      this.setState({ errorMessage: "E-mail obrigatório." });
+      return;
+    } else {
+      if (!regexEmail.test(this.state.email)) {
+        this.setState({ errorMessage: "Formato do e-mail deve ser: ###@###.com" });
+        return;
+      }
+    }
+
+    if (this.state.password.length == 0) {
+      this.setState({ errorMessage: "Senha obrigatória." });
+    } else if (this.state.password.length < 7) {
+      this.setState({ errorMessage: "A senha deve ter pelo menos 7 caracteres." });
+    } else {
+      if (!regexOneCharAndOneDigit.test(this.state.password)) {
+        this.setState({ errorMessage: "A senha deve conter pelo menos um caracter e um dígito"});
+      } else {
+        this.setState({ errorMessage: "Senha e login corretos!" });
+      }
+    }
+  }
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -17,6 +48,8 @@ export default class HelloWorldApp extends Component {
           <TextInput
             style={{height: 40, width: 200, borderColor: 'gray', borderWidth: 1, marginBottom: 15 }}
             autoCompleteType='email'
+            autoCapitalize = 'none'
+            onChangeText={(inputText) => this.setState({email: inputText})}
           />
 
           <Text style={{ fontSize: 15, marginBottom: 5 }}>Senha:</Text>
@@ -24,17 +57,24 @@ export default class HelloWorldApp extends Component {
           <TextInput
             style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1, marginBottom: 15 }}
             autoCompleteType='password'
+            autoCapitalize = 'none'
             underlineColorAndroid='transparent'
             secureTextEntry={true}
+            onChangeText={(inputText) => this.setState({password: inputText})}
           />
 
         </View>
 
-        <View>
+        <View style={{ marginBottom: 15 }}>
           <Button
+              onPress={() => this.validateEmailAndPassword()}
               title="Entrar"
               color="#9400D3"
             />
+        </View>
+
+        <View>
+          <Text style={{ fontSize: 15, color:"#FF0000" }}>{this.state.errorMessage}</Text>
         </View>
       </View>
     );
