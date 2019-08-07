c215c56d3b374df07bd2143572f094e6528a58bd
marcosppm <marcos_ppm@live.com>
2019-08-07 10:06:13 -0300

    Correções pós Code Review. Refatoração do código

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,44 +1,37 @@
 import React from 'react'
 import { Component } from 'react';
-import { Text, View, TextInput, Button } from 'react-native';
+import { Text, View, TextInput, Button, ViewStyle, TextStyle } from 'react-native';
 
-interface IState {
+interface HelloWorldAppState {
   email?: string;
   password?: string;
   errorMessage?: string;
 }
 
-interface IProps {}
+interface HelloWorldAppProps {}
 
-export default class HelloWorldApp extends Component<IProps, IState> {
-  constructor(props: IProps) {
+export default class HelloWorldApp extends Component<HelloWorldAppProps, HelloWorldAppState> {
+  constructor(props: HelloWorldAppProps) {
     super(props);
     this.state = {email:"", password:"", errorMessage:""};
   }
 
-  validateEmailAndPassword(): void {
+  private validateEmailAndPassword(): void {
     const regexEmail:RegExp = /([A-Za-z])+@([A-Za-z])+.com/gm;
     const regexOneCharAndOneDigit:RegExp = /(?=.*?[0-9])(?=.*?[A-Za-z]).+/gm;
-    if (this.state.email.length == 0) {
+    if (!this.state.email) {
       this.setState({ errorMessage: "E-mail obrigatório." });
-      return;
-    } else {
-      if (!regexEmail.test(this.state.email)) {
-        this.setState({ errorMessage: "Formato do e-mail deve ser: ###@###.com" });
-        return;
-      }
-    }
-
-    if (this.state.password.length == 0) {
+    } else if (!regexEmail.test(this.state.email)) {
+      this.setState({ errorMessage: "Formato do e-mail deve ser: ###@###.com" });
+    } else if (!this.state.password) {
       this.setState({ errorMessage: "Senha obrigatória." });
     } else if (this.state.password.length < 7) {
       this.setState({ errorMessage: "A senha deve ter pelo menos 7 caracteres." });
+    } else if (!regexOneCharAndOneDigit.test(this.state.password)) {
+      this.setState({ errorMessage: "A senha deve conter pelo menos uma letra e um dígito"});
     } else {
-      if (!regexOneCharAndOneDigit.test(this.state.password)) {
-        this.setState({ errorMessage: "A senha deve conter pelo menos um caracter e um dígito"});
-      } else {
-        this.setState({ errorMessage: "Senha e login corretos!" });
-      }
+      this.setState({ errorMessage: ""});
+      // login successful
     }
   }
 
@@ -82,7 +75,7 @@ export default class HelloWorldApp extends Component<IProps, IState> {
             />
         </View>
 
-        <View>
+        <View style={{ alignItems: 'baseline' }}>
           <Text style={{ fontSize: 15, color:"#FF0000" }}>{this.state.errorMessage}</Text>
         </View>
       </View>
